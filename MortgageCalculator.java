02437b3547c6d7887586fe5d5a2f5037d9eaf279
MD-Sakil-Hasan <mdsakilhasan833@gmail.com>
2021-04-07 23:34:51 +0600

    Number Validation added

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -1,25 +1,38 @@
 import java.util.Scanner;
 
 public class MortgageCalculator {
-    public static void main(String[] args){
-        final byte parcentage = 100;
-        final byte totalMonths = 12;
+    public static void main(String[] args) {
+        final byte PARCENTAGE = 100;
+        final byte TOTAL_MONTH = 12;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Principal: ");
         int principal = input.nextInt();
 
+        while (principal < 0) {
+            System.out.println("Please enter a positive decimal number_ ");
+            principal = input.nextInt();
+        }
+
         System.out.print("Interest Rate: ");
-        float interestRate = (float)input.nextDouble();
+        float annualInterest = input.nextFloat();
+        while (annualInterest < 0) {
+            System.out.println("Please enter a valid Interest Rate_ ");
+            annualInterest = input.nextFloat();
+        }
 
-        interestRate = interestRate/parcentage/totalMonths;
+        float interestRate = annualInterest / PARCENTAGE / TOTAL_MONTH;
         System.out.print("Period (Years): ");
-        byte years = input.nextByte();
-        int period = years * totalMonths;
+        short years = input.nextShort();
+        while (years < 0 || years > 30) {
+            System.out.println("Year must be positive decimal number and not more than 30 ");
+            years = input.nextShort();
+        }
+        int period = years * TOTAL_MONTH;
 
-        float n = (float)Math.pow((1+interestRate), period);
-        
-        float mortgage = (principal * (interestRate * n))/(n-1);
+        float n = (float) Math.pow((1 + interestRate), period);
+
+        float mortgage = (principal * (interestRate * n)) / (n - 1);
         System.out.printf("Mortgage: %.2f", mortgage);
     }
 }
